98f03ac7263a6d7f437f84242051a026049471e3
Fredy Jimenez <fjimenez@tmg.io>
2024-08-01 21:01:34 -0500

    Home and initial navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import Reservations from "./routes/Reservations";
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <Router>
+      <div>
+        <h1>Little Lemon App setup</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/reservations" element={<Reservations />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
